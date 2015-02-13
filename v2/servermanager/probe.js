c7e9b1be2f90b96aafc5b86135c41b5218a11af4
Adrian Lee <jun.irok@gmail.com>
2015-02-12 23:53:59 -0800

    Added debug logs for redis function calls

--- v2/servermanager/probe.js
+++ v2/servermanager/probe.js
@@ -3,23 +3,24 @@ var redis = require('redis');
 var r = redis.createClient(6379, 'bojap.com', {
     auth_pass: "01895v7nh10234985y19034v85vyb01945v8"
 });
+// config
+var debug = false;
 // srcds
 var srcds = require('srcds-info');
 
 function pingServer(serverId) {
     r.mget([
-        ["server", serverId, "ip"].join(":"),
-        ["server", serverId, "port"].join(":")
+        ["server", serverId, "ip"].join(":"), ["server", serverId, "port"].join(":")
     ], function(err, res) {
         var client = srcds(res[0], res[1]);
         client.info(function(err, info) {
             if (err) {
-                console.error(err);
+                if (debug) console.error(err);
             } else {
-                console.log(res[0], res[1], "has a heartbeat");
+                if (debug) console.log(res[0], res[1], "has a heartbeat");
                 var keyServerStatus = ["server", serverId, "serverStatus"].join(":");
-                r.set(keyServerStatus, 1, redis.print);
-                r.expire(keyServerStatus, 30, redis.print);
+                r.set(keyServerStatus, 1, redisResponse);
+                r.expire(keyServerStatus, 30, redisResponse);
             }
             client.close();
         });
@@ -35,5 +36,10 @@ function probe() {
         }
     });
 }
+// redis repsonse handler
+function redisResponse(err, res) {
+    if (debug && err) console.error(err);
+    if (debug && res) console.log(res);
+}
 probe();
-setInterval(probe, 20 * 1000); 
+setInterval(probe, 20 * 1000);
